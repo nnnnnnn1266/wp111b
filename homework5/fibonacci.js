a6fab1e1c93e776a4f231159dfd9e87846691b37
nnnnnnn1266 <99786134+nnnnnnn1266@users.noreply.github.com>
2023-06-17 02:21:59 +0800

    Create fibonacci.js

---
+++ homework5/fibonacci.js
@@ -0,0 +1,16 @@
+  //參考chatGPT
+function fibonacci(n) {
+    if (n <= 0) {
+      return 0;
+    } else if (n === 1) {
+      return 1;
+    } else {
+      const result = fibonacci(n - 1) + fibonacci(n - 2);
+      return result;
+    }
+  }
+  
+  const num = 10;
+  const fibonacciNumber = fibonacci(num);
+  console.log(`Fibonacci(${num}) = ${fibonacciNumber}`);
+  
