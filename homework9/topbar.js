b4f5016a2570942571d9719b10fdebe9cd9ac45a
nnnnnnn1266 <99786134+nnnnnnn1266@users.noreply.github.com>
2023-06-17 02:57:02 +0800

    Create topbar.js

---
+++ homework9/topbar.js
@@ -0,0 +1,69 @@
+function myFunction() 
+{
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+  
+window.onclick = function (e) 
+{
+    if (!e.target.matches('.dropbtn')) 
+    {
+        var myDropdown = document.getElementById("myDropdown");
+
+        if (myDropdown.classList.contains('show')) 
+        {
+            myDropdown.classList.remove('show');
+        }
+    }
+}
+
+let main = document.querySelector('#main')
+
+let pages = 
+{
+  '#login':`
+  <h1>登入</h1>
+  <input type="text" placeholder="帳號"/><br/>
+  <input type="text" placeholder="密碼"/><br/>
+  <a href="#home">確認</a>
+  `,
+
+  '#home':`
+  <div id="main" class="d1">
+    <h1>首頁</h1>
+    <p>歡迎來到這個網站</p>
+
+    <div id="main" class="d2">
+
+  </div>
+  `,
+
+  '#logout':`
+  <a href="#home">登出</a>
+  `,
+
+  '#signup':`
+  <h2>註冊</h2>
+  <input type="text" placeholder="姓名"/><br>
+  <input type="mail" placeholder="電子信箱或手機號碼"/><br>
+  <input type="password" placeholder="設定密碼"/><br>
+  <input type="money" placeholder="銀行卡號"/><br>
+  <label for="birthdaytime">出生年月日:</label><br>
+  <input required type="date"><br>
+  <br>
+  <label for="gender">性別:</label><br>
+  <input type="radio" name="gender" value="male">
+  <label for="male">男性</label>
+  <input type="radio" name="gender" value="female">
+  <label for="female">女性</label> 
+  <input type="radio" name="gender" value="other">
+  <label for="other">其他</label><br>
+  <br>
+  <input type="button" onclick="alert('確定')" value="送出">
+  `,
+}
+
+window.onhashchange = function () 
+{
+    let hash = window.location.hash
+    main.innerHTML = pages[hash]
+}
